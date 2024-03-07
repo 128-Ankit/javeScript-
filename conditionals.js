3a1816226c1db35e324e2f3ab39df1dc20bb45a7
Ankit Kumar <abhishekankit128@gmail.com>
2024-03-08 00:50:53 +0530

    adding about ? oprator

--- conditionals.js
+++ conditionals.js
@@ -1,9 +1,14 @@
-const age = prompt("How old are you?");
+// const age = prompt("How old are you?");
 
-if(age < 18) {
-    console.log("You are a minor.");
-} else if (age == 18) {
-    console.log("You are an adult.")
-} else {
-    console.log("You are an elderly person.");
-}
+// if(age < 18) {
+//     console.log("You are a minor.");
+// } else if (age == 18) {
+//     console.log("You are an adult.")
+// } else {
+//     console.log("You are an elderly person.");
+// }
+let age = 18;
+
+let accessAllowed = (age > 18) ? true : false;
+
+console.log(`Access allowed: ${accessAllowed}`)
